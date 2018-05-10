b60d730c94d1aa09f997cc403d3be50d985b77f2
SGfdo <39162183+SGfdo@users.noreply.github.com>
2018-05-10 20:07:11 +0530

    Add files via upload

---
+++ token.php
@@ -0,0 +1,18 @@
+<?php
+	class Token{
+		public static function generate($session_id){
+			$_SESSION['token'] = sha1($session_id);
+			
+		}
+
+		public static function check($token){
+			if (isset($_SESSION['token']) && $token === $_SESSION['token']) {
+				unset($_SESSION['token']);
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+	}
+?>
